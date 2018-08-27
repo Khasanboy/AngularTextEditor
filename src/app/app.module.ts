cd4c1543b9a40ab51362de3626b308defd0fd30d
Khasanboy Akbarov <Khasanboy.akbarov@thronegaming.com>
2018-08-27 09:59:56 +0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {QuillModule} from 'ngx-quill';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    QuillModule
   ],
   providers: [],
   bootstrap: [AppComponent]
